9a81075549c23705f3da7f6163f4134ec91dfa1f
BetjaderOD <20213tn032@utez.edu.mx>
2024-08-17 15:26:46 -0600

    [ADD Estilos en complete login

--- src/components/forms/CompleteLoginForm.jsx
+++ src/components/forms/CompleteLoginForm.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { Button, Container, Form, InputGroup } from "react-bootstrap";
-import { Spinner } from "@nextui-org/react";
+import { Container, Form, InputGroup } from "react-bootstrap";
+import { Input, Spinner } from "@nextui-org/react";
 import { passwordRegex } from "../../utils/regex";
 import api from "../../config/axios/client-gateway";
 import endpoints from "../../utils/endpoints";
 import Swal from "sweetalert2";
+import "../../css/auth/Login.css";
+import { Button } from "@mui/material";
 
 export default function CompleteLoginForm() {
   const [username, setUsername] = useState("");
@@ -17,20 +19,6 @@ export default function CompleteLoginForm() {
   const [isLoading, setIsLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const styles = {
-    container: {
-      backgroundColor: "#F9F9F9",
-      borderRadius: "25px",
-      padding: "50px",
-      display: "flex",
-      justifyContent: "center",
-      width: "50%",
-    },
-    form: {
-      width: "80%",
-    },
-  };
 
   useEffect(() => {
     if (isSubmitted) {
@@ -67,12 +55,12 @@ export default function CompleteLoginForm() {
         setIsLoading(true);
         console.log("entre");
         const response = await api.doPost(endpoints.SetPasswordFunction, {
-          username: localStorage.getItem('email'),
-          temporary_password: localStorage.getItem('password'),
+          username: localStorage.getItem("email"),
+          temporary_password: localStorage.getItem("password"),
           new_password: password,
         });
-        
-        if(response.status === 200){
+
+        if (response.status === 200) {
           Swal.fire({
             title: "¡Éxito!",
             text: response.data.message,
@@ -80,111 +68,123 @@ export default function CompleteLoginForm() {
             confirmButtonText: "Cerrar",
             allowEscapeKey: false,
           }).then(async () => {
-
             Swal.fire({
-              html: '<h3>Iniciando sesión...</h3><span class="spinner-border spinner-border-sm"></span>', 
+              html: '<h3>Iniciando sesión...</h3><span class="spinner-border spinner-border-sm"></span>',
               showConfirmButton: false,
               allowOutsideClick: false,
               allowEscapeKey: false,
             });
 
-            const responseLogin = await api.doPost(endpoints.LoginFunction, { 
-              username: localStorage.getItem('email'),
-              password: password
+            const responseLogin = await api.doPost(endpoints.LoginFunction, {
+              username: localStorage.getItem("email"),
+              password: password,
             });
             if (responseLogin.status === 200) {
-              localStorage.removeItem('email');
-              localStorage.removeItem('password');
-              localStorage.setItem("accessToken", responseLogin.data.access_token);
-              localStorage.setItem("refreshToken", responseLogin.data.refresh_token);
+              localStorage.removeItem("email");
+              localStorage.removeItem("password");
+              localStorage.setItem(
+                "accessToken",
+                responseLogin.data.access_token
+              );
+              localStorage.setItem(
+                "refreshToken",
+                responseLogin.data.refresh_token
+              );
               localStorage.setItem("idToken", responseLogin.data.id_token);
               localStorage.setItem("role", responseLogin.data.role);
               localStorage.setItem("userId", response.data.id);
               window.location.href = "/movies";
             }
-          })
+          });
         }
-      } catch (error) {} finally {
+      } catch (error) {
+      } finally {
         setIsLoading(false);
-      };
+      }
     }
   };
 
   return (
-    <Container fluid style={styles.container}>
-      <Form onSubmit={handleSubmitForm} style={styles.form}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Nombre de usuario:</Form.Label>
-          <Form.Control
-            className={validUsername ? "" : "border-error"}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Nombre de usuario"
-          />
-          { !validUsername ?  
-          <Form.Text className="message-error">
-            Nombre de usuario inválido.
-          </Form.Text>
-          : ""
-          }
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Nueva contraseña:</Form.Label>
-          <InputGroup>
-            <Form.Control
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type={showPassword ? "text" : "password"}
-              placeholder="Contraseña"
-              className={validPassword ? "" : "border-error"}
-            />
-            <Button
-              variant="secondary"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </Button>
-          </InputGroup>
-          {validPassword ? "" :
-            <Form.Text className="message-error">
-              Contraseña no válida.
+    <>
+      <Container fluid>
+        <h1 className="title movier">MOVIER</h1>
+        <h4 className="subtitle">Completa tu registro</h4>
+        <Container className="login-container">
+          <Form onSubmit={handleSubmitForm}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Input
+                type="email"
+                label="Usuario"
+                placeholder="Ingresa tu correo electrónico"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                errorMessage={
+                  validUsername ? "" : "Nombre de usuario inválido."
+                }
+                isInvalid={!validUsername}
+                color="secondary"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Input
+                type={showPassword ? "text" : "password"}
+                label="Contraseña"
+                placeholder="Ingresa tu contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                endContent={
+                  <Button onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </Button>
+                }
+                color="secondary"
+                errorMessage={validPassword ? "" : "Contraseña no válida."}
+                isInvalid={!validPassword}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicConfirmedPassword">
+              <Input
+                type={showPassword ? "text" : "password"}
+                label="Confirmar contraseña"
+                placeholder="Confirma tu contraseña"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                endContent={
+                  <Button onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </Button>
+                }
+                color="secondary"
+                errorMessage={
+                  validConfirmPassword ? "" : "Las contraseñas no coinciden."
+                }
+                isInvalid={!validConfirmPassword}
+              />
+            </Form.Group>
+            <Form.Text className="mb-3">
+              Recuerda que tu contraseña debe contener mínimo 8 caracteres con
+              al menos: <br />
+              - Una letra mayúscula. <br />
+              - Una letra minúscula. <br />
+              - Un número. <br />
+              - Un carácter especial. <br />
             </Form.Text>
-          }
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicConfirmedPassword">
-          <Form.Label>Confirmar nueva contraseña:</Form.Label>
-          <InputGroup>
-            <Form.Control
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              type={showPassword ? "text" : "password"}
-              placeholder="Confirmar contraseña"
-              className={validConfirmPassword ? "" : "border-error"}
-            />
             <Button
-              variant="secondary"
-              onClick={() => setShowPassword(!showPassword)}
+              className="mt-3"
+              variant="contained"
+              disabled={isLoading}
+              style={{ width: "100%" }}
+              type="submit"
             >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
+              {!isLoading ? (
+                "Completar registro"
+              ) : (
+                <Spinner color="secondary" />
+              )}
             </Button>
-          </InputGroup>
-          { validConfirmPassword ? "" :
-            <Form.Text className="message-error">
-              Las contraseñas no coinciden.
-            </Form.Text>
-          }
-        </Form.Group>
-        <Form.Text className="mb-3">
-          Recuerda que tu contraseña debe contener mínimo 8 caracteres con al menos: <br />
-          - Una letra mayúscula. <br />
-          - Una letra minúscula. <br />
-          - Un número. <br />
-          - Un carácter especial. <br />
-        </Form.Text>
-        <Button className="mt-3" disabled={isLoading} style={{ width: "100%" }} type="submit">
-          {!isLoading ? "Completar registro" : <Spinner color="secondary" />}
-        </Button>
-      </Form>
-    </Container>
+          </Form>
+        </Container>
+      </Container>
+    </>
   );
 }
